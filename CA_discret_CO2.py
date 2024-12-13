ad463e24651ae437ba0aa5a88bcd9a97df9c47c6
Moges <Moges.ash@gmail.com>
2024-12-13 08:56:23 +0100

    Code description added

--- CA_discret_CO2.py
+++ CA_discret_CO2.py
@@ -2,7 +2,28 @@
 """
 Created on Thu Dec 12 22:29:21 2024
 
-@author: angel
+@author: Moges Retta
+
+Integrate the equation for the net hydration rate of CO2 with respect to CO2
+% Calculate hydration rate and gradient of hydration at the current iteration
+% Source terms are linearized using Picard's method of source term linearization.
+% https://www.cfd-online.com/Wiki/Source_term_linearization
+
+Inputs
+CA : a geometrical domain where CA hydration is assumed
+ka: turnover rate of carbonic anhydrase enzyme (1/s)
+kC : Michaelis-Menten constants of carbonic anhydrase hydration (mol m-3)
+keq : equilibrium constant for CA
+kHCO3: Michaelis-Menten constants of carbonic anhydrase dehydration (mol m-3)
+pro : concentration of H+ ions (mol/m3)
+u0_CO2 : CO2 concentration at the current iteration (umol/m3)
+u0_HCO3 : bicarbonate concentration at the current iteration(umol/m3)
+Xa : concentratiion of CA (mol/m3)
+
+Outputs
+CA_r: net hydration of CO2
+div_CA : first derivative of CA_r with respect to CO2
+
 """
 def CA_discret_CO2(u0_CO2, u0_HCO3, pro, CA, ka, C_CA, K_const, kC, kHCO3):
     Xa = CA * C_CA
--- CA_discret_HCO3.py
+++ CA_discret_HCO3.py
@@ -2,7 +2,25 @@
 """
 Created on Thu Dec 12 22:30:01 2024
 
-@author: angel
+@author: Moges Retta
+
+Integrate the equation for the net hydration rate of CO2 with respect to bicarbonate
+
+Inputs
+CA : a geometrical domain where CA hydration is assumed
+ka : turnover rate of carbonic anhydrase enzyme (1/s)
+kC : Michaelis-Menten constants of carbonic anhydrase hydration (mol m-3)
+k_eq : equilibrium constant for CA
+k_HCO3 : Michaelis-Menten constants of carbonic anhydrase dehydration (mol m-3)
+H : cocentration of H+ ions (mol/m3), pro
+u0_CO2 : CO2 concentration (umol/m3)
+u0_HCO3 : bicarbonate concentration (umol/m3)
+Xa : concentratiion of CA (mol/m3)
+
+Outputs
+CA_r : net hydration of CO2
+div_CA : first derivative of CA_r with respect to HCO3
+
 """
 def CA_discret_HCO3(u0_CO2, u0_HCO3, pro, CA, ka, C_CA, K_const, kC, kHCO3):
     Xa = CA * C_CA
