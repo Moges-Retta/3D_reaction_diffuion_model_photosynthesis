9d867d5a36576a08d57f7ec5afc59aa142e0891b
Moges <Moges.ash@gmail.com>
2024-12-13 10:17:25 +0100

    imported h_mem_o2, corrected indentation

--- coeff_mod_chloro_vacuole_O2.py
+++ coeff_mod_chloro_vacuole_O2.py
@@ -7,6 +7,7 @@ Created on Thu Dec 12 22:35:14 2024
 
 import numpy as np
 import math
+import h_mem_O2
 
 def coeff_mod_chloro_vacuole_O2(i, j, k, i1, j1, k1, D, T, geom, chloro, vacuole, air, meso_bundle, u_O2, epid, meso_ori):
     tcw_m = 0.161e-6 # mesophyll cell wall - N1A1 leaves
@@ -43,20 +44,21 @@ def coeff_mod_chloro_vacuole_O2(i, j, k, i1, j1, k1, D, T, geom, chloro, vacuole
         
         #Adding the resistance to membrane and cellwall to the cell of bs and
         #mc
-        id1 = meso_bundle[i][j][k]
-        id2 = meso_bundle[i1][j1][k1]
-hm = h_mem_O2(u_O2(i, j, k), T)
-hm1 = h_mem_O2(u_O2(i1, j1, k1), T)
-h_cm_tam = min(hm, hm1)
-h_cm_tam2 = h_cm_tam
-r_cm = 1 / (h_cm_tam * Hen_O2 * dx) # bundle sheath plasma membrane
-r_cm2 = 1 / (h_cm_tam2 * Hen_O2 * dx) # mesophyll plasma membrane
-if id1 != id2:
-    if id1 == 0 or id2 == 0:
-        if id1 > 100 or id2 > 100: # bs exposed to air
-            a = 1 / (1 / a + r_cw3 + r_cm)
-        elif id1 < 100 or id2 < 100: # mc exposed to air
-            a = 1 / (1 / a + r_cw + r_cm2)
+    id1 = meso_bundle[i][j][k]
+    id2 = meso_bundle[i1][j1][k1]
+    hm = h_mem_O2(u_O2(i, j, k), T)
+    hm1 = h_mem_O2(u_O2(i1, j1, k1), T)
+    h_cm_tam = min(hm, hm1)
+    h_cm_tam2 = h_cm_tam
+    r_cm = 1 / (h_cm_tam * Hen_O2 * dx) # bundle sheath plasma membrane
+    r_cm2 = 1 / (h_cm_tam2 * Hen_O2 * dx) # mesophyll plasma membrane
+
+    if id1 != id2:
+        if id1 == 0 or id2 == 0:
+            if id1 > 100 or id2 > 100: # bs exposed to air
+                a = 1 / (1 / a + r_cw3 + r_cm)
+            elif id1 < 100 or id2 < 100: # mc exposed to air
+                a = 1 / (1 / a + r_cw + r_cm2)
     else:
         if id1 > 100:
             if id2 > 100:
@@ -68,48 +70,49 @@ if id1 != id2:
                 a = 1 / (1 / a + r_pd) # mc bs interface
             else:
                 a = 1 / (1 / a + r_cw + r_cw + r_cm2 + r_cm2) # mc mc interface
-id1 = meso_ori[i][j][k] + epid[i][j][k] + air[i][j][k]
-id2 = meso_ori[i1][j1][k1] + epid[i1][j1][k1] + air[i][j][k]
 
-if id1 != id2:
-    if id1 == 0 or id2 == 1 or id1 == 1 or id2 == 0:
-        a = 1/(1/a + r_cw + r_cm) # pore epid interface
-    elif id1 > 0 and id2 == 1 or id1 == 1 and id2 > 0:
-        a = 1/(1/a + r_cw + r_cm + r_cw + r_cm2) # Mc epid interface
+    id1 = meso_ori[i][j][k] + epid[i][j][k] + air[i][j][k]
+    id2 = meso_ori[i1][j1][k1] + epid[i1][j1][k1] + air[i][j][k]
 
-tam1 = chloro[i][j][k]
-tam2 = chloro[i1][j1][k1]
+    if id1 != id2:
+        if id1 == 0 or id2 == 1 or id1 == 1 or id2 == 0:
+            a = 1/(1/a + r_cw + r_cm) # pore epid interface
+        elif id1 > 0 and id2 == 1 or id1 == 1 and id2 > 0:
+            a = 1/(1/a + r_cw + r_cm + r_cw + r_cm2) # Mc epid interface
 
-if tam1 != tam2:
-    hm = hm1 = 0
-    if tam1 > 0:
-        hm = h_mem_O2(u_O2[i][j][k], T)
-    if tam2 > 0:
-        hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
-    r_mem = 1/(hm * Hen_O2 * dx)
-    r_mem1 = 1/(hm1 * Hen_O2 * dx)
-    a = 1/(1/a + 2*r_mem + 2*r_mem1)
+    tam1 = chloro[i][j][k]
+    tam2 = chloro[i1][j1][k1]
 
-tam1 = meso_ori[i][j][k] * chloro[i][j][k]
-tam2 = meso_ori[i1][j1][k1] * chloro[i1][j1][k1]
-
-if tam1 > 0 and tam2 > 0:
     if tam1 != tam2:
-        hm = h_mem_O2(u_O2[i][j][k], T)
-        hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
+        hm = hm1 = 0
+        if tam1 > 0:
+            hm = h_mem_O2(u_O2[i][j][k], T)
+        if tam2 > 0:
+            hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
         r_mem = 1/(hm * Hen_O2 * dx)
         r_mem1 = 1/(hm1 * Hen_O2 * dx)
         a = 1/(1/a + 2*r_mem + 2*r_mem1)
 
-tam1 = vacuole[i][j][k]
-tam2 = vacuole[i1][j1][k1]
+    tam1 = meso_ori[i][j][k] * chloro[i][j][k]
+    tam2 = meso_ori[i1][j1][k1] * chloro[i1][j1][k1]
 
-if tam1 != tam2:
-    hm = hm1 = 0
-    if tam1 > 0:
-        hm = h_mem_O2(u_O2[i][j][k], T)
-    if tam2 > 0:
-        hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
-    r_mem = 1/(hm * Hen_O2 * dx)
-    r_mem1 = 1/(hm1 * Hen_O2 * dx)
-    a = 1/(1/a + r_mem + r_mem1)
+    if tam1 > 0 and tam2 > 0:
+        if tam1 != tam2:
+            hm = h_mem_O2(u_O2[i][j][k], T)
+            hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
+            r_mem = 1/(hm * Hen_O2 * dx)
+            r_mem1 = 1/(hm1 * Hen_O2 * dx)
+            a = 1/(1/a + 2*r_mem + 2*r_mem1)
+
+    tam1 = vacuole[i][j][k]
+    tam2 = vacuole[i1][j1][k1]
+    
+    if tam1 != tam2:
+        hm = hm1 = 0
+        if tam1 > 0:
+            hm = h_mem_O2(u_O2[i][j][k], T)
+        if tam2 > 0:
+            hm1 = h_mem_O2(u_O2[i1][j1][k1], T)
+        r_mem = 1/(hm * Hen_O2 * dx)
+        r_mem1 = 1/(hm1 * Hen_O2 * dx)
+        a = 1/(1/a + r_mem + r_mem1)
